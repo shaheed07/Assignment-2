1b04a7d82062aaf671a7bb30b41accf0c8d70622
M .SHAHID <dhonishahid12@gmail.com>
2023-02-09 04:07:02 -0800

    22

---
+++ StringLowerExample.java
@@ -0,0 +1,10 @@
+public class StringLowerExample {
+    public static void main(String args[]) {
+
+
+        String s1 = "JAVATPOINT HELLO stRIng";
+        String s1lower = s1.toLowerCase();
+        System.out.println(s1lower);
+    }
+}
+
